02269347a1673cf85e9be3ba1aeb64149540bade
alviennn <alvien.rnp@gmail.com>
2024-11-23 23:21:39 +0700

    Menambahkan middleware body-parser dan menjalankan server pada port yang ditentukan

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ const PORT = 8000;
 
 app.use(bodyParser.json());
 app.use(express.json());
+
+app.use(bodyParser.json());
+app.listen(PORT, () =>
+    console.log(`Server berjalan di port: http://localhost:${PORT}`)
+);
